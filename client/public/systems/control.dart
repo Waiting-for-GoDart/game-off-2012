6c77d9898aa6911b7dd3e5faa2a795395f06756d
Joel Griffith <joel@joel-griffith.com>
2012-11-18 08:35:21 -0500

    update control system

--- client/public/systems/control.dart
+++ client/public/systems/control.dart
@@ -1,23 +1,58 @@
 part of system;
 
 class Control extends System {
+  static final int KEY_UP = 0;
+  static final int KEY_DOWN = 1;
+
   CanvasElement canvas;
-  
+  int keyW, keyA, keyD;
+
   Control(this.canvas){
-    update();
+    document.on.keyDown.add((KeyboardEvent e) {
+      if (isKeyA(e.keyCode)) {
+        keyA = KEY_DOWN;
+      } else if (iskeyD(e.keyCode)) {
+        keyD = KEY_DOWN;
+      } else if (isKeyW(e.keyCode)) {
+        keyW = KEY_DOWN;
+      }
+    });
+
+    document.on.keyUp.add((KeyboardEvent e) {
+      if (isKeyA(e.keyCode)) {
+        keyA = KEY_UP;
+      } else if (isKeyD(e.keyCode)) {
+        keyD = KEY_UP;
+      } else if (isKeyW(e.keyCode)) {
+        keyW = KEY_UP;
+      }
+    });
   }
   
   void update() {
-    document.on.keyPress.add((KeyboardEvent e) {
-      for (var entity in entities) {
-        if (e.keyCode == KeyCode.W || e.keyCode == 119) {
+    for (var entity in entities) {
+        if (keyW == KEY_DOWN) {
           entity.velocity.y -= 10;
-        } else if (e.keyCode == KeyCode.A || e.keyCode == 97) {
+        }
+        if (keyA == KEY_DOWN) {
           entity.velocity.x -= 10;
-        } else if (e.keyCode == KeyCode.D || e.keyCode == 100) {
+        }
+        if (keyD == KEY_DOWN) {
           entity.velocity.x += 10;
         }
       }
     });
+  }
+
+  bool isKeyA(int keyCode) {
+    return (keyCode == KeyCode.A || keyCode == 97);
+  }
+
+  bool isKeyD(int keyCode) {
+    return (keyCode == KeyCode.D || keyCode == 100);
+  }
+
+  bool isKeyW(int keyCode) {
+    return (keyCode == KeyCode.W || keyCode == 119);
   }
 }
