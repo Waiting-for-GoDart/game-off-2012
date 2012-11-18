b27f85110fe6a9c518c424fc57c886848a6879ad
Daniel Motles <seltom.dan@gmail.com>
2012-11-17 19:23:59 -0500

    parse json response and connect to correct URL

--- client/WaitingForGodart.dart
+++ client/WaitingForGodart.dart
@@ -9,12 +9,12 @@ class WaitingForGodart {
     _ws = new WebSocket( "ws://$addr" );
     StringInputStream stdinput = new StringInputStream(stdin);
     stdinput.onLine = () => _ws.send( JSON.stringify( { 'message':stdinput.readLine() }) );
-    _ws.onmessage = (e) => print( e.data );
+    _ws.onmessage = (e) => print( JSON.parse( e.data )['message'] );
   }
   
 }
 
 
 void main() {
-  WaitingForGodart wfgd = new WaitingForGodart( "127.0.0.1:9001" );
+  WaitingForGodart wfgd = new WaitingForGodart( "127.0.0.1:9001/game" );
 }
