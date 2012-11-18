930f8050f4107812b4e928ad33b2e509fec2ac07
Daniel Motles <seltom.dan@gmail.com>
2012-11-18 03:30:08 -0500

    starting network abstraction layer

--- client/public/WaitingForGodart.dart
+++ client/public/WaitingForGodart.dart
@@ -1,31 +1,18 @@
+library WaitingForGodart;
 import 'dart:html';
 import 'dart:json';
 
+part 'network_socket.dart';
+
 class WaitingForGodart {
-  WebSocket _ws;
-  TextAreaElement _textarea;
-  InputElement _input;
+ 
   
-  WaitingForGodart( String addr ) {
-    _ws = new WebSocket( "ws://$addr" );
-    _textarea = query("#output");
-    _input = query("#inputbox");
-    query("#submit").on.click.add( (e) {
-        var outmap = {'Message': _input.value };
-        var jsonreq = JSON.stringify( outmap );
-        print( jsonreq );
-        _ws.send( jsonreq );
-      }
-    );
-    _ws.on.open.add( (e) => print("connected") );
-    _ws.on.close.add( (e) => print("closed") );
-    _ws.on.error.add( (e) => print("error") );
-    _ws.on.message.add( (e) => _textarea.addText( "${JSON.parse( e.data )['Message']}\n" )  );
+  void initialize() {
+    
   }
    
 }
 
 
 void main() {
-  WaitingForGodart wfgd = new WaitingForGodart( "${document.domain}:9001/game" );
 }
