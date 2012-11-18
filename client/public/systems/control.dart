0929a61c3cd174a8b26362c7d6713afe5390c678
Joel Griffith <joel@joel-griffith.com>
2012-11-18 07:18:37 -0500

    working? control system... I guess

--- client/public/systems/control.dart
+++ client/public/systems/control.dart
@@ -2,21 +2,22 @@ part of system;
 
 class Control extends System {
   CanvasElement canvas;
-  Movement movementSystem;
   
-  Control(Movement movement) {
+  Control() {
     canvas = Utilities.canvas();
-    movementSystem = movement;
   }
   
   void update() {
-    canvas.on.keyDown.add((KeyboardEvent e) {
-      for (var entity in entities) {}
+    canvas.on.keyPress.add((KeyboardEvent e) {
+      for (var entity in entities) {
+        if (e.keyCode == KeyCode.W) {
+          entity.velocity.y = entity.velocity.y + 10
+        } else if (e.keyCode == KeyCode.A) {
+          entity.velocity.x = entity.velocity.x - 10
+        } else if (e.keyCode == KeyCode.D) {
+          entity.velocity.x = entity.velocity.x + 10
+        }
+      }
     });
-    
-    
-  }
-  void updateEntity(KeyboardEvent e, var entity) {
-
   }
 }
